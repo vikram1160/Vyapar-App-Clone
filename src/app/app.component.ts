db2685307193dfd158ba47c02bb8805a61c0b88f
Vikram Golait <vikram.golait@ikione.com>
2025-03-26 16:07:54 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ButtonModule,InputTextModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
